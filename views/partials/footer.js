31d8cb4120a8fa34222c3f54313616f25e642057
luka003-hub <kimathiluka03@gmail.com>
2024-10-11 10:17:12 -0700

    update

---
+++ views/partials/footer.js
@@ -0,0 +1,45 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row">
+            <!-- About Section -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">About Us</h5>
+                <p>
+                    We are a leading telemedicine platform providing accessible healthcare solutions.
+                </p>
+            </div>
+
+            <!-- Contact Section -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <ul class="list-unstyled mb-0">
+                    <li><i class="fas fa-phone-alt"></i>+254748801144</li>
+                    <li><i class="fas fa-envelope"></i> support@telemedicine.com</li>
+                    <li><i class="fas fa-map-marker-alt"></i> Nairobi</li>
+                </ul>
+            </div>
+
+            <!-- Social Media Links -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Follow Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-dark"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+                    <li><a href="#" class="text-dark"><i class="fab fa-twitter"></i> Twitter</a></li>
+                    <li><a href="#" class="text-dark"><i class="fab fa-instagram"></i> Instagram</a></li>
+                    <li><a href="#" class="text-dark"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="text-center p-3 bg-dark text-white">
+        © 2024 Telemedicine Platform. All Rights Reserved.
+    </div>
+</footer>
+
+
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+
+<link rel="stylesheet" href="/styles/main.css">
